6faac407e9ec96e1737c9e3ee39e3b91a50045a7
Sam Nicholls <sam.nicholls@drpgroup.com>
2020-11-20 18:11:01 +0000

    Updates

---
+++ precache-manifest.bbb1a49256d4afca38803c1a94904d30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d374067053e10be33498636ae3a9db9",
+    "url": "/fake-store/index.html"
+  },
+  {
+    "revision": "61120ad7e20954cbf812",
+    "url": "/fake-store/static/css/main.f27c22ef.chunk.css"
+  },
+  {
+    "revision": "a84caaf8ba605dc9d98e",
+    "url": "/fake-store/static/js/2.922cea55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fake-store/static/js/2.922cea55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61120ad7e20954cbf812",
+    "url": "/fake-store/static/js/main.d3dbf3b1.chunk.js"
+  },
+  {
+    "revision": "c27f1583c187f7081ab4",
+    "url": "/fake-store/static/js/runtime-main.854ea354.js"
+  }
+]);
